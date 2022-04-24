7d72b5669a392c9983fd71b50903354e075d115c
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-04-24 22:22:54 +0200

    npm run nx -- migrate --run-migrations

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/apps/react-app',
     '<rootDir>/libs/react-app/state/store',
     '<rootDir>/libs/react-app/state/reducers',
